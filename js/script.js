bc7c73bc776c798448ff3144d0c38caf8d38cfed
Alyona <mysiura148121620@gmail.com>
2018-11-26 16:28:14 +0200

    Lesson3

---
+++ js/script.js
@@ -0,0 +1,64 @@
+window.addEventListener('DOMContentLoaded', function() {
+  let products = document.querySelectorAll('.product'),
+      buttons = document.getElementsByTagName("button"),
+      open = document.getElementsByClassName("open")[0];
+
+  function createCart() {
+    let cart = document.createElement("div"),
+        field = document.createElement('div'),
+        heading = document.createElement('h2'),
+        close = document.createElement("button");
+
+  cart.classList.add("cart");
+  field.classList.add("cart-field");
+  close.classList.add("close");
+
+  close.textContent = "Закрыть";
+  heading.textContent = "В нашей корзине";
+
+  document.body.appendChild(cart);
+  cart.appendChild(heading);
+  cart.appendChild(field);
+  cart.appendChild(close);
+  }
+  createCart();
+
+
+  let field = document.querySelector('.cart-field'),
+      cart = document.querySelector('.cart'),
+      close = document.querySelector('.close');
+
+  for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      let item = products[i].cloneNode(true),
+        btn = item.querySelector('button');
+
+      btn.textContent = " удалить";
+      field.appendChild(item);
+      products[i].remove();
+    });
+  }
+
+  for (let k = 0; k < buttons.length; k++) {
+    buttons[k].addEventListener('click', function() {
+      let item = products[k].cloneNode(true),
+        btn = item.querySelector('button');
+
+      btn.textContent = "купить";
+      shop.appendChild(item);
+      products[k].remove();
+  });
+  }
+
+  function openCart() {
+    cart.style.display = "block";
+  }
+  function closeCart() {
+    cart.style.display = "none";
+  }
+
+  open.addEventListener('click', openCart);
+  close.addEventListener('click', closeCart);
+});
+
+
